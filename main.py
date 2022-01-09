5727082cad68ce52e212963115de574c1f57a84a
Newman <NewmanLai@ami.com>
2022-01-09 14:46:32 +0800

    update final game

--- main.py
+++ main.py
@@ -19,19 +19,51 @@ pygame.display.set_caption("貪食蛇遊戲")
 pygame.display.update()
 
 gameRun = True
+gameOver = False
 clock = pygame.time.Clock()
 
 while gameRun:
     clock.tick(10)
     for event in pygame.event.get():
+        #事件捕捉
         if event.type == pygame.QUIT:
             gameRun = False
+        if event.type == pygame.KEYDOWN:#按下按鈕
+            if event.key == pygame.K_UP:#上
+                if snake_obj.dir != "下":
+                    snake_obj.dir = "上"
+            elif event.key == pygame.K_DOWN:#下
+                if snake_obj.dir != "上":
+                    snake_obj.dir = "下"
+            elif event.key == pygame.K_RIGHT:#右
+                if snake_obj.dir != "左":
+                    snake_obj.dir = "右"
+            elif event.key == pygame.K_LEFT:#左
+                if snake_obj.dir != "右":
+                    snake_obj.dir = "左"
     screen.fill(white)
     app_obj.createApple()
-    snake_obj.move()
+    snake_obj.move(app_obj) #蛇移動的程式碼
     
+    result = snake_obj.checkGameOver()
+    if result == True:#GameOver後的行為
+        gameOver = True
+        #snake_obj = snake.Snake()
 
-    
+    #Game Over的畫面    
+    while gameOver:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameOver = False
+                gameRun = False
+            if event.type == pygame.KEYDOWN:
+                #按下ESC鍵
+                if event.key == pygame.K_ESCAPE:
+                    gameOver = False
+                    snake_obj = snake.Snake()
+        screen.fill((255,0,0))
+        pygame.display.update()
+                    
     pygame.draw.rect(screen,
                      (255,0,0),
                      [app_obj.x, app_obj.y,10,10],
--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ class Snake:
         self.body = [[400,300],[390,300],[380,300]]
         self.dir = "右"
 
-    def move(self):
+    def move(self, food):
         if self.dir == "右":
             self.head[0] = self.head[0] + 10
         elif self.dir == "左":
@@ -17,4 +17,19 @@ class Snake:
 
         #將頭的資料放到身體的第一個索引值
         self.body.insert(0, list(self.head))
-        self.body.pop() #將最後一個身體移除
+        #吃到食物
+        if self.head[0] == food.x and self.head[1] == food.y:
+            food.changeStatus(False)
+        else: #沒有吃到食物
+            self.body.pop() #將最後一個身體移除
+
+    #如果return值為True:GameOver, False:Game alive
+    def checkGameOver(self):
+        if self.head[0] < 0 or self.head[0] > 800:
+            return True
+        if self.head[1] < 0 or self.head[1] > 600:
+            return True
+        if self.head in self.body[1:]:
+            return True
+        return False
+        
